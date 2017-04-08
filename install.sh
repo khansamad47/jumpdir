342c5074d5f655a8f92d11773e4d39a4320006c7
Samad Khan <khan.samad47@gmail.com>
2017-04-08 13:41:47 -0400

    Bug fix

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-SCRIPT_DIR=$HOME/bin
-SCRIPT_DIR=$(dirname $(realpath $0))
+SCRIPT_DIR=$HOME/scripts
+SOURCE_DIR=$(dirname $(realpath $0))
 SCRIPT_FILE='jumpdir.sh'
 BASH_RC=$HOME/.bashrc
 mkdir -vp $SCRIPT_DIR
-cp $SCRIPT_DIR/$SCRIPT_FILE $SCRIPT_DIR/.
+cp $SOURCE_DIR/$SCRIPT_FILE $SCRIPT_DIR/.
 
 if [ ! -f $BASH_RC ]; then
-    echo "Cannot find bash rc!"
+    echo "Error! Cannot find .bashrc file!"
     exit 1
 fi
 
 LINE="source $SCRIPT_DIR/$SCRIPT_FILE"
-grep -q -F "$LINE" $BASH_RC || echo "$LINE" >> $BASH_RC || source $BASH_RC
+grep -q -F "$LINE" $BASH_RC || echo "$LINE" >> $BASH_RC && source $BASH_RC
 
