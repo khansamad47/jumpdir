7bc5c3be740482883e1b5fcaff35bfcea2f1922f
Samad Khan <khan.samad47@gmail.com>
2017-04-08 13:28:28 -0400

    Install to script dir

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-BIN_DIR=$HOME/bin
+SCRIPT_DIR=$HOME/bin
 SCRIPT_DIR=$(dirname $(realpath $0))
 SCRIPT_FILE='jumpdir.sh'
 BASH_RC=$HOME/.bashrc
-mkdir -vp $BIN_DIR
-cp $SCRIPT_DIR/$SCRIPT_FILE $BIN_DIR/.
+mkdir -vp $SCRIPT_DIR
+cp $SCRIPT_DIR/$SCRIPT_FILE $SCRIPT_DIR/.
 
 if [ ! -f $BASH_RC ]; then
     echo "Cannot find bash rc!"
     exit 1
 fi
 
-LINE="source $BIN_DIR/$SCRIPT_FILE"
+LINE="source $SCRIPT_DIR/$SCRIPT_FILE"
 grep -q -F "$LINE" $BASH_RC || echo "$LINE" >> $BASH_RC || source $BASH_RC
 
