8dd577635d967b5fcb8a94d47987bbe879af42b4
Samad Khan <khan.samad47@gmail.com>
2024-03-01 09:52:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-SCRIPT_DIR=$HOME/scripts
+SCRIPT_DIR=/usr/local/bin
 SOURCE_DIR=$(dirname $(realpath $0))
 SCRIPT_FILE='jumpdir.sh'
 BASH_RC=$HOME/.bashrc
 mkdir -vp $SCRIPT_DIR
-cp $SOURCE_DIR/$SCRIPT_FILE $SCRIPT_DIR/.
+cp $SOURCE_DIR/$SCRIPT_FILE $SCRIPT_DIR/jd
 
 if [ ! -f $BASH_RC ]; then
     echo "Error! Cannot find .bashrc file!"
     exit 1
 fi
 
-LINE="source $SCRIPT_DIR/$SCRIPT_FILE"
+LINE="source $SCRIPT_DIR/jd"
 grep -q -F "$LINE" $BASH_RC || echo "$LINE" >> $BASH_RC && source $BASH_RC
 
